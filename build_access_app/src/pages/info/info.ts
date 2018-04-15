d0f4b7153167da9b919a6107ddab6f607d9e3cf0
Michelle Dudley <25540019+michdud@users.noreply.github.com>
2018-04-15 03:16:41 -0500

    smaller style changes and adding content to the intro page

--- build_access_app/src/pages/info/info.ts
+++ build_access_app/src/pages/info/info.ts
@@ -17,15 +17,6 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: 'info.html',
 })
 export class InfoPage {
-  	parkingQuestions: Observable<any[]>;
-  	buildingQuestions: Observable<any[]>;
-  	roomQuestions: Observable<any[]>;
-
-  	constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
-  		this.parkingQuestions = this.firebaseProvider.getParkingQuestions().valueChanges();
-  		this.buildingQuestions = this.firebaseProvider.getBuildingQuestions().valueChanges();
-  		this.roomQuestions = this.firebaseProvider.getRoomQuestions().valueChanges();
-  	}
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad InfoPage');
