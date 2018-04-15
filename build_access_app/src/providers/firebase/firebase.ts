8d64d15a8e4a092aaafc92e6bdcd8ed4fb938298
ahelfins <ahelfins@macalester.edu>
2018-04-15 08:59:27 -0500

    made a schools list in firebase.ts and only push song if not in the list already

--- build_access_app/src/providers/firebase/firebase.ts
+++ build_access_app/src/providers/firebase/firebase.ts
@@ -11,8 +11,13 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 @Injectable()
 export class FirebaseProvider {
 
+  schools: Array<any>;
+
   constructor(public afDB: AngularFireDatabase) {
     console.log('Hello FirebaseProvider Provider');
+    this.schools = new Array();
+    this.makeSchoolsList();
+    console.log("schools list " + this.schools[0]);
   }
 
   getParkingQuestions() {
@@ -26,9 +31,25 @@ export class FirebaseProvider {
   getRoomQuestions(){
   	return this.afDB.list('/RoomQs/');
   }
+  private makeSchoolsList(){
+    let i = 0;
+    this.afDB.list("/Schools").valueChanges()
+      .subscribe(list =>{
+        list.forEach(item => {
+          this.schools[i] = item['school'];
+          console.log(this.schools[i]);
+          i++;
+        });
+      });
+  }
 
   pushNewSchool(school){
-    this.afDB.list('/Schools/').push({ school: school });
+    if(this.schools.indexOf(school)==-1){
+      console.log(school + " is not in "+this.schools);
+      this.schools[this.schools.length]=school;
+      console.log("last school in list "+this.schools[this.schools.length]);
+      this.afDB.list('/Schools/').push({ school: school });
+    }
   }
 
 }
