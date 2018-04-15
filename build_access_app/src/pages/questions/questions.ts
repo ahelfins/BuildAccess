48e366161893a3994d02368f90e877412d8a3888
Michelle Dudley <25540019+michdud@users.noreply.github.com>
2018-04-14 23:32:14 -0500

    Adding stuff to the questions page

--- build_access_app/src/pages/questions/questions.ts
+++ build_access_app/src/pages/questions/questions.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the QuestionsPage page.
@@ -14,12 +17,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'questions.html',
 })
 export class QuestionsPage {
+  	parkingQuestions: Observable<any[]>;
+  	buildingQuestions: Observable<any[]>;
+  	roomQuestions: Observable<any[]>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  	constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
+  		this.parkingQuestions = this.firebaseProvider.getParkingQuestions().valueChanges();
+  		this.buildingQuestions = this.firebaseProvider.getBuildingQuestions().valueChanges();
+  		this.roomQuestions = this.firebaseProvider.getRoomQuestions().valueChanges();
+  	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuestionsPage');
-  }
 
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad InfoPage');
+        document.getElementById("question").innerHTML = "hello";
+
+        var arr = [];
+        this.parkingQuestions.subscribe(q => {q.forEach(q => {
+        	arr.push(q);
+        	arr.push(1);
+        })});
+
+        console.log(arr);
+
+    }
 }
